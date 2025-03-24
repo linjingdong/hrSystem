a482e82d705e0d01eb5741957fee3acab1ee178c
lin_jd <lin_jd@haici.com>
2025-03-25 00:03:38 +0800

    feat:优化注册登录功能 ⭐添加获取系统配置

--- hr-common/src/main/java/com/lin/hr/common/constants/AccountConstant.java
+++ hr-common/src/main/java/com/lin/hr/common/constants/AccountConstant.java
@@ -1,11 +1,15 @@
 package com.lin.hr.common.constants;
 
+import com.lin.hr.common.enums.user.UserContactTypeEnum;
+import com.lin.hr.common.enums.user.UserTypeEnum;
+
 /**
  * @author Lin_jd
  * @version V1.0.0
  * @since 2025/3/22 21:04
  **/
 public class AccountConstant {
+    public static final String ROBOT_UID = UserContactTypeEnum.USER.getPrefix() + "robot";
     public static final Integer LENGTH_11 = 11;
     public static final Integer LENGTH_20 = 20;
 }
